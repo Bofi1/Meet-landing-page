87514567c1354cc657a3f55822c5e821ede27b08
Bofi1 <angelborque03github@gmail.com>
2025-08-15 22:03:16 -0700

    tablet done and desktop almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      screens: {
+        textTablet: "440px",
+      },
       fontFamily: {
         redhat: ["'Red Hat'", "sans-serif"],
       },
